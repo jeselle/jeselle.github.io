2486e50135e7008ad72c25f2469315f31566b898
K. Jeselle Clark <jeselleclark@ks-macbook-pro.myfiosgateway.com>
2020-11-06 21:25:25 -0500

    Building in docs dir

---
+++ docs/precache-manifest.27b0bfc7a60e2f2bf27bda01072e9dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9398e13bae1993b30f7a1e8260e3327d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc0c83ebbdde1f10c70b",
+    "url": "/static/css/main.b1a79951.chunk.css"
+  },
+  {
+    "revision": "10e86ea0960559415961",
+    "url": "/static/js/2.72a2d166.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.72a2d166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc0c83ebbdde1f10c70b",
+    "url": "/static/js/main.f3efe10e.chunk.js"
+  },
+  {
+    "revision": "89ee3db22aa98fd8a45c",
+    "url": "/static/js/runtime-main.c199c862.js"
+  }
+]);
