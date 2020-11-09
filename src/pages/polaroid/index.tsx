57dc7c22bd9436c530879c552cc7611cb4f9d432
lance13c <pianoman13c@gmail.com>
2020-11-08 22:46:59 -0500

    notebook WIP

--- src/pages/polaroid/index.tsx
+++ src/pages/polaroid/index.tsx
@@ -1,7 +1,10 @@
 import React, {useEffect} from 'react';
 import styled from 'styled-components';
 import $ from "jquery";
+import { updateDepth, getViewNumber } from './helper';
+import Hash from './hash';
 import "../../libs/turn.js";
+import "./notebook.css";
 import './index.scss';
 
 const MainBackground = styled.div`
@@ -24,6 +27,10 @@ const NoteBookContainer = styled.div`
   transform: rotateZ(-30deg);
 `
 
+interface Book {
+  [property: string]: any
+}
+
 const options = {
   width: 850,
   height: 600,
@@ -34,11 +41,103 @@ const options = {
   // gradients: !$.isTouch,
   when: {
     // @ts-ignore
-    turned: function(e, page) {
+    turning: function(e, page, view) {
+				
+				var book: Book = $(this),
+					currentPage = book.turn('page'),
+					pages = book.turn('pages');
+
+				if (currentPage>3 && currentPage<pages-3) {
+				
+					if (page==1) {
+						book.turn('page', 2).turn('stop').turn('page', page);
+						e.preventDefault();
+						return;
+					} else if (page==pages) {
+						book.turn('page', pages-1).turn('stop').turn('page', page);
+						e.preventDefault();
+						return;
+					}
+				} else if (page>3 && page<pages-3) {
+					if (currentPage==1) {
+						book.turn('page', 2).turn('stop').turn('page', page);
+						e.preventDefault();
+						return;
+					} else if (currentPage==pages) {
+						book.turn('page', pages-1).turn('stop').turn('page', page);
+						e.preventDefault();
+						return;
+					}
+				}
+
+				updateDepth(book, page);
+				
+				if (page>=2)
+					$('.sj-book .p2').addClass('fixed');
+				else
+					$('.sj-book .p2').removeClass('fixed');
+
+				if (page<book.turn('pages'))
+					$('.sj-book .p111').addClass('fixed');
+				else
+					$('.sj-book .p111').removeClass('fixed');
+
+				Hash.go('page/'+page).update();
+					
+			},
+
+      // @ts-ignore
+			turned: function(e, page, view) {
+
+				var book = $(this);
+
+				if (page==2 || page==3) {
           // @ts-ignore
-      console.log("Current view: ", $(this).turn("view"));
-    }
-  }
+					book.turn('peel', 'br');
+				}
+
+				updateDepth(book);
+				// @ts-ignore
+				// $('#slider').slider('value', getViewNumber(book, page));
+        // @ts-ignore
+				book.turn('center');
+
+			},
+
+      // @ts-ignore
+			start: function(e, pageObj) {
+        // @ts-ignore
+				// moveBar(true);
+
+			},
+
+      // @ts-ignore
+			end: function(e, pageObj) {
+			
+				var book = $(this);
+
+				updateDepth(book);
+
+				setTimeout(function() {
+          // @ts-ignore
+					// $('#slider').slider('value', getViewNumber(book));
+
+				}, 1);
+
+				// moveBar(false);
+
+			},
+
+      // @ts-ignore
+			missing: function (e, pages) {
+
+				for (var i = 0; i < pages.length; i++) {
+          // @ts-ignore
+					addPage(pages[i], $(this));
+				}
+
+			}
+		}
 };
 
 export interface PolaroidPageProps {
