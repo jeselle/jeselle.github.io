3a09c153a9623fd170686eb20396c1505512974e
lance13c <pianoman13c@gmail.com>
2020-11-08 21:51:17 -0500

    notebook now flips due to arrow keys

--- src/pages/polaroid/index.tsx
+++ src/pages/polaroid/index.tsx
@@ -2,41 +2,44 @@ import React, {useEffect} from 'react';
 import styled from 'styled-components';
 import $ from "jquery";
 import "../../libs/turn.js";
+import './index.scss';
 
 const MainBackground = styled.div`
   position: absolute;
   width: 100%;
   height: 100%;
-  background-image: url('/public/images/red-wood.jpg');
+  background-image: url('/images/red-wood.jpg');
   background-size: cover;
 `
 const PageContainer = styled.div`
   width: 100vw;
   height: 100vh;
-  overflow: none;
+  overflow: hidden;
 `;
 
-const NoteBook = styled.div`
-  width: 300px;
-  height: 400px;
+const NoteBookContainer = styled.div`
+  position: relative;
+  top: 25vh;
+  left: 40vw;
+  transform: rotateZ(-30deg);
 `
 
-// const options = {
-//   width: 800,
-//   height: 600,
-//   autoCenter: true,
-//   display: "double",
-//   acceleration: true,
-//   elevation: 50,
-//   // gradients: !$.isTouch,
-//   when: {
-//     // @ts-ignore
-//     turned: function(e, page) {
-//           // @ts-ignore
-//       console.log("Current view: ", $(this).turn("view"));
-//     }
-//   }
-// };
+const options = {
+  width: 850,
+  height: 600,
+  autoCenter: false,
+  display: "double",
+  acceleration: true,
+  elevation: 50,
+  // gradients: !$.isTouch,
+  when: {
+    // @ts-ignore
+    turned: function(e, page) {
+          // @ts-ignore
+      console.log("Current view: ", $(this).turn("view"));
+    }
+  }
+};
 
 export interface PolaroidPageProps {
   
@@ -46,26 +49,34 @@ const PolaroidPage: React.FC<PolaroidPageProps> = () => {
 
   useEffect(() => {
     // @ts-ignore
-    $("#notebook").turn({ 
-      width: 400,
-      height: 300,
-      autoCenter: true
-    });
+    $("#notebook").turn(options);
+
+    document.addEventListener('keydown', (e) => {
+      if (e.key == 'ArrowRight') {
+        // @ts-ignore
+         $("#notebook").turn("next");
+         console.log("right")
+      } else if (e.key == 'ArrowLeft') {
+        // @ts-ignore
+        $("#notebook").turn("previous");
+        console.log("left")
+      }
+    })
   }, [])
 
   return (
     <PageContainer>
       <MainBackground />
-      <NoteBook id="notebook">
-        <div className="hard"> Turn.js </div>
-        <div className="hard"> Turn.js </div>
-        <div> Hello my name is Jeselle </div>
-        <div> This is page two </div>
-        <div> This is page three </div>
-        <div> This is page four </div>
-        <div className="hard"></div>
-        <div className="hard"></div>
-      </NoteBook>
+      <NoteBookContainer>
+        <div id="notebook">
+          <div className="page"> Hello my name is Jeselle </div>
+          <div className="page"> This is page two </div>
+          <div className="page"> This is page three </div>
+          <div className="page"> This is page four </div>
+          <div className="hard"></div>
+          <div className="hard"></div>
+        </div>
+      </NoteBookContainer>
     </PageContainer>
    );
 }
